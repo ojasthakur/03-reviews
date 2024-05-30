05ac180929332e3893d8b07672789f84544ac8a0
Ojas Singh Thakur <ojasthakurx@192.168.1.2>
2024-05-31 00:18:45 +0530

    Project complete

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,68 @@
+import { useState } from "react";
+import people from "./data";
+import { FaChevronLeft, FaChevronRight, FaQuoteRight } from "react-icons/fa";
+
 const App = () => {
-  return <h2>Reviews Starter</h2>;
+  const [index, setIndex] = useState(0);
+  const { name, job, image, text } = people[index];
+
+  const nextPerson = () => {
+    setIndex((currenIndex) => {
+      if (currenIndex === people.length - 1) {
+        return 0;
+      } else {
+        return currenIndex + 1;
+      }
+    });
+  };
+  const prevPerson = () => {
+    setIndex((currenIndex) => {
+      if (currenIndex === 0) {
+        return people.length - 1;
+      } else {
+        return currenIndex - 1;
+      }
+    });
+  };
+  const randomPerson = () => {
+    setIndex((currenIndex) => {
+      let randomNumber = Math.floor(Math.random() * people.length);
+      while (randomNumber === currenIndex) {
+        randomNumber = Math.floor(Math.random() * people.length);
+      }
+      return randomNumber;
+    });
+  };
+
+  return (
+    <main>
+      <article className="review">
+        <div className="img-container">
+          <img src={image} alt={name} className="person-img" />
+          <span className="quote-icon">
+            <FaQuoteRight />
+          </span>
+        </div>
+        <h4 className="author">{name}</h4>
+        <p className="job">{job}</p>
+        <p className="info">{text}</p>
+        <div className="btn-container">
+          <button type="button" className="prev-btn" onClick={prevPerson}>
+            <FaChevronLeft />
+          </button>
+          <button type="button" className="prev-btn" onClick={nextPerson}>
+            <FaChevronRight />
+          </button>
+        </div>
+        <button
+          type="button"
+          className="btn btn-hipster"
+          onClick={randomPerson}
+        >
+          surprise me
+        </button>
+      </article>
+    </main>
+  );
 };
 export default App;
